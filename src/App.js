5febfa5c800e735490e2a1560a6e628696593315
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-13 23:14:30 +0530

    Created Contructor to the App class
    > Added addItem method to add todo
    > Added deleteItem method to remove todo
    > Added updateInput method to copy the input field value to this.state.newItem

--- src/App.js
+++ src/App.js
@@ -3,6 +3,51 @@ import logo from "./AP_logo.svg";
 import "./App.css";
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			newItem: "",
+			list: []
+		};
+	}
+
+	//Creating an item in Todo list
+	addItem(todoValue) {
+		if (todoValue !== "") {
+			const newItem = {
+				id: Date.now(),
+				value: todoValue,
+				isDone: false
+			};
+
+			//Getting list array from the object
+			const list = [...this.state.list];
+			list.push(newItem);
+
+			//Setting state of the list array
+			this.setState({
+				newItem: "",
+				list
+			});
+		}
+	}
+
+	//Deleting an item in Todo list
+	deleteItem(id) {
+		//Getting list array from the object
+		const list = [...this.state.list];
+		//Skipping the item with the id given as an argument
+		const updatedList = list.filter(item => item.id !== id);
+
+		//Setting state of the list array
+		this.setState({list: updatedList});
+	}
+
+	//TODO: Comeback here
+	updateInput(input){
+		this.setState({newItem: input})
+	}
+
 	render() {
 		return (
 			<section className="main-component">
@@ -22,8 +67,15 @@ class App extends React.Component {
 							type="text"
 							className="input-text"
 							placeholder="Write a TODO"
+							required
+							value = {this.state.newItem}
+							onChange = {event => this.updateInput(event.target.value)}
 						/>
-						<button className="btn-add">Add TODO</button>
+						<button 
+						className="btn-add"
+						onClick = {() => this.addItem(this.state.newItem)}
+						disabled = {!this.state.newItem.length}
+						>Add TODO</button>
 					</div>
 					<div className="list">
 						<ul>
